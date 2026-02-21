72eb20e3144c1437f98985af8ebe32d880d07e95
akqqa <alexk2002@gmail.com>
2026-02-21 13:13:30 +0000

    added pixel to character mapping generation

--- Image2ASCII/src/image2ASCII.cpp
+++ Image2ASCII/src/image2ASCII.cpp
@@ -9,6 +9,9 @@
 using namespace cimg_library;
 using namespace std;
 
+const float CHAR_ASPECT = 0.5f; // Characters are ~half as wide as they are tall
+const int OUTPUT_WIDTH = 60;
+
 // Algorithm:
 // read characters, create a mapping of 256 range to characters evenly based on how many there are#
 // calculate size of image, and split into average brightness of each size so one section = one character
@@ -23,13 +26,61 @@ vector<string> getCharacterSet(string filename) {
     return characterSet;
 }
 
-map<int, string> mapCharacterDensity(vector<string> characterSet) {
-    map<int, string> mapping = {{1, "one"}, {2, "two"}};
+// Resizes the image so that one pixel = one character. Has the bonus of performing the average operation for us
+CImg<unsigned char> resizeImage(CImg<unsigned char> image, int outputWidth) {
+    int imgWidth = image.width();
+    int imgHeight = image.height();
+    float aspectRatio = (float)imgHeight / imgWidth;
+
+    int outputHeight = outputWidth * aspectRatio * CHAR_ASPECT;
+
+    return image.resize(outputWidth, outputHeight);
+}
+
+map<int, string> mapCharacterDensity(vector<string> characterSet, CImg<unsigned char> image, bool scaleContrast) {
+    // Get brightness range of image
+    image = image.RGBtoYCbCr().channel(0);
+
+    int maxBrightness = 0;
+    int minBrightness = 256;
+    if (scaleContrast) {
+        cimg_forXY(image, x, y) {
+            unsigned char value = image(x, y);
+            if (value > maxBrightness) {
+                maxBrightness = value;
+            }
+            if (value < minBrightness) {
+                minBrightness = value;
+            }
+        }
+    }
+
+    cout << "max brightness: " << maxBrightness << "\n";
+    cout << "min brightness: " << minBrightness << "\n";
+
+    // Divide range by number of character levels
+    int numLevels = characterSet.size();
+    double intervalSize = double(maxBrightness - minBrightness + 1) / numLevels;
+
+    // Create map - unsure what will happen - can the case of the top level only being say 1 value due to rounding? dont want this
+    map<int, string> mapping;
+    for (int level = 0; level < numLevels; ++level) {
+        int upper = int(minBrightness + intervalSize * (level  + 1)) - 1;
+        // Clamp last range to maxBrightness
+        if (level == numLevels - 1) {
+            upper = maxBrightness;
+        }
+
+        mapping.insert({upper, characterSet[level]});
+    }
+
     return mapping;
 }
 
+// 1st arg: character set
+// 2nd arg: image
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
+    if (argc != 3) {
         cout << "Incorrect number of arguments\n";
         return 1;
     }
@@ -40,12 +91,11 @@ int main(int argc, char* argv[]) {
         cout << chars << "\n";
     }
 
-    map<int, string> mapping = mapCharacterDensity(characterSet);
-    cout << "first map: " << mapping[1] << "\n";
+    CImg<unsigned char> image(argv[2]);
+    CImg<unsigned char> resizedImage = resizeImage(image, OUTPUT_WIDTH);
 
-    // CImg<unsigned char> image("marathon.jpg");
-    // CImgDisplay main_disp(image, "this is the image");
-    // while(true) {
-        
-    // }
+    map<int, string> mapping = mapCharacterDensity(characterSet, resizedImage, true);
+
+    for (auto it = mapping.begin(); it != mapping.end(); ++it) 
+        cout << it->first << " " << it->second << endl;
 }
